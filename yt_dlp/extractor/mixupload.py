fe9776ee9239c14e48a0b8194a58e0ebf6bec1ab
Jayson Lennon <jayson@jaysonlennon.dev>
2022-08-24 13:22:26 -0700

    Remove debugging stuff

--- yt_dlp/extractor/mixupload.py
+++ yt_dlp/extractor/mixupload.py
@@ -15,10 +15,8 @@ class MixUploadIE(InfoExtractor):
     def _real_extract(self, url):
         video_id = self._match_id(url)
         api_url = f'https://mixupload.com/api2/track/{video_id}/info'
-        print(api_url)
 
         data = self._download_json(api_url, video_id)
-        print(data)
 
         artist = data['track']['artist']
         title = data['track']['title']
@@ -35,75 +33,3 @@ class MixUploadIE(InfoExtractor):
             'url': download_url,
             'ext': 'mp3'
         }
-
-"""
-{
-    "status": "ok",
-    "track": {
-        "id": "6706910",
-        "title": "Back to 1994",
-        "post": "U7EpicTrance",
-        "album": "Back to 1994",
-        "artist": "Andy Jornee Feat. Victoriya",
-        "label": "Universe7",
-        "url": "andy-jornee-feat.-victoriya-back-to-1994-6706910",
-        "year": "2022",
-        "tags": "7,4,15,19,17",
-        "uid": 554253,
-        "lock_holder": "0",
-        "waveform": "ACFGGHIJKLMTY[[UUUUUVVUUTTUUVWVWVVVXWURRQQUVYZZ\\`[`aacc`eehgfhgegfdhffjighgfhhfhf_Vhnmlnlmieiic]V\\ZZXURPWZYXVNLKIGFGGHIKLQSWX[\\[[[ZTPPOOORVWVUUVWVWXZ\\\\ZXZXYXXXVVTTSSTSRQQRRSUWXXUU\\\\[ZZYXYYXXUVX[[\\\\\\\\[\\][]]\\_gfffeccaa]ZYYUZgjihjhhhgghhgjhgihghhjhgge]bjjijihiigiigggijihiigjigibcjiiijhjhfihggakfdd_\\ZUSRPPP`hfhgehfdfedf^fihegfgfeefebeWZecacb_a`\\`^Z_\\X\\ZWYXSWSPRGFCAAAAAAAAA",
-        "adddate": "2022-08-01 19:58:50",
-        "free_block": "1",
-        "creation": "1",
-        "official": "1",
-        "legal": "1",
-        "duration": 370,
-        "xtrack": 0,
-        "xmix": 0,
-        "type": "track",
-        "sizebyte": 14813517,
-        "srv": "4",
-        "styleparts": "52,61",
-        "quantityLike": 3,
-        "quantityListen": 75,
-        "quantityComment": 0,
-        "quantityDownload": 2,
-        "quantityPlaylist": 0,
-        "quantityShares": 0,
-        "urlname": "",
-        "hash_high": "",
-        "typeDownload": "PREMIUM",
-        "name": "Label Worx",
-        "status": "label",
-        "file": "https:\/\/n1.mixupload.com\/media\/track_api\/6706\/910\/ltrack.mp3",
-        "file_320": "",
-        "cover": "https:\/\/static.mixupload.com\/n1\/media\/track\/6706\/910\/cover.jpg?184",
-        "cover_max": "https:\/\/static.mixupload.com\/n1\/media\/track\/6706\/910\/cover-max.jpg?184",
-        "avatarUser": "https:\/\/static.mixupload.com\/n0\/media\/userphoto\/2020-04-07\/5e8c5d7d0b92c-original.jpg?728",
-        "lastComment": [],
-        "userDisliked": false,
-        "userLiked": false,
-        "userPlaylist": false,
-        "userListened": false,
-        "file_types": {
-            "mp3low": "Mp3 128",
-            "mp3": "Mp3 320",
-            "flac": "44100hz FLAC",
-            "wav": "44100hz WAV"
-        },
-        "file_format": "FLAC",
-        "label_urlname": "universe7",
-        "label_name": "Universe7",
-        "artists": [
-            {
-                "urlname": "andy-jornee",
-                "name": "Andy Jornee"
-            },
-            {
-                "urlname": "victoriya",
-                "name": "Victoriya"
-            }
-        ]
-    }
-}
-"""
